514eafe69b2e1a16dc793b06b91f59aec87038fb
sancharimukherjee <58549135+sancharimukherjee@users.noreply.github.com>
2020-01-04 12:44:10 +0530

    Add files via upload

---
+++ truyumServlet/truyum/src/com/cognizant/truyum/util/DateUtil.java
@@ -0,0 +1,30 @@
+package com.cognizant.truyum.util;
+
+import java.text.ParseException;
+
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+
+public class DateUtil {
+
+	public Date convertToDate(String input) {
+
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+
+		try {
+			Date dateOfLaunch = format.parse(input);
+			return dateOfLaunch;
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public String convertToString(Date input) {
+		SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String date = format.format(input);
+		return date;
+	}
+
+}
